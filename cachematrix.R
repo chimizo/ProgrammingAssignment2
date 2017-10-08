b1588a65468da0ac6c210394913323d68947c3e0
chimizo <31078962+chimizo@users.noreply.github.com>
2017-10-08 12:38:47 -0300

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,6 @@
+#this is Programmin Assignment 2 - Lexical Scoping lesson
 
+#This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -17,6 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   
